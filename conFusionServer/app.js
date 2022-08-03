8cb392213356e6dad1faed3ac21df7d06cb6aa0e
Ajeet Kumar Soni <ajeet.krsoni.cse17@itbhu.ac.in>
2022-08-03 13:01:27 +0530

    express-generator REST api

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishRouter = require('./routes/dishRouter');
+var dishIdRouter = require('./routes/dishIdRouter');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishRouter);
+app.use('/dishes', dishIdRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
