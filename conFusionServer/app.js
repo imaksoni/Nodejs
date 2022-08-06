eeae406735d4c2e1e055224bcabf85300c4cdc16
Ajeet Kumar Soni <ajeet.krsoni.cse17@itbhu.ac.in>
2022-08-06 15:49:07 +0530

    Express REST api with mongoose part 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -7,7 +7,19 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-var dishIdRouter = require('./routes/dishIdRouter');
+
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url);
+
+connect.then((db)=> {
+  console.log("Connected correctly to server");
+}, (err)=> {
+  console.log(err);
+});
 
 var app = express();
 
@@ -24,7 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
-app.use('/dishes', dishIdRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
