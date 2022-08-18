86bfececbb432ef549cd8e1c5636398a132505d0
Ajeet Kumar Soni <ajeet.krsoni.cse17@itbhu.ac.in>
2022-08-18 23:12:13 +0530

    facebook oAuth2

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var jwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -35,4 +36,40 @@ exports.jwtPassport = passport.use(new jwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if(req.user.admin)
+        next();
+    else {
+        var err = new Error("You are not authorized to perform this operation!");
+        err.status = 403;
+        return next(err);
+    }
+};
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({facebookId: profile.id}, (err, user) => {
+        if(err) {
+            return done(err, false);
+        }
+        if(!err && user!==null){
+            return done(null, user);
+        } else {
+            user = new User({
+                 username: profile.displayName});
+                 user.facebookId = profile.id;
+                 user.firstname = profile.name.givenName;
+                 user.lastname = profile.name.familyName;
+                 user.save((err, user) => {
+                    if(err)
+                        return done(err, false);
+                    else
+                        return done(null, user);
+                 })
+        }
+    })
+}));
     
