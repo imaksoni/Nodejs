36809c60d16feea4df8dd8759727bb11782f3d7d
Ajeet Kumar Soni <ajeet.krsoni.cse17@itbhu.ac.in>
2022-08-18 14:58:07 +0530

    file upload

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -12,6 +12,7 @@ var config = require('./config');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -53,6 +54,7 @@ app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/dishes', dishRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
